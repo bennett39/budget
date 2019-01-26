563fd8d5ac897a5688b48e4568f500be8e25dea4
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-01-26 11:55:47 -0500

    Add BudgetConfig to installed apps

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'budget.apps.BudgetConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
