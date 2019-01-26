e68612e60aae010a5bf97f1838b60b56e9edce1f
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-01-26 13:24:54 -0500

    Implement personal capital login email/password entry

--- budget/forms.py
+++ budget/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
 class SmsAuthenticationForm(forms.Form):
-    sms_code = forms.CharField(label='sms code', max_length=4)
+    sms_code = forms.CharField(label="SMS Code", max_length=4)
+
+class PersonalCapitalLoginForm(forms.Form):
+    pc_email = forms.EmailField(label="Email")
+    pc_password = forms.CharField(label="Password",\
+            widget=forms.PasswordInput)
--- budget/views.py
+++ budget/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.template import loader
-
-from .forms import SmsAuthenticationForm
+ 
+from .fetchdata import fetch_accounts_json, fetch_transactions_json
+from .forms import SmsAuthenticationForm, PersonalCapitalLoginForm
+#  from .secrets import pc_email, pc_password
 
 from datetime import datetime, timedelta
 from personalcapital import PersonalCapital, RequireTwoFactorException,\
@@ -13,37 +15,59 @@ import json
 pc = PersonalCapital()
 
 def index(request):
-    session = request.session
-    session['pc_email'] = ""
-    session['pc_password'] = ""
-    
-    try:
-        pc.login(session['pc_email'], session['pc_password'])
-    except RequireTwoFactorException:
-        pc.two_factor_challenge(TwoFactorVerificationModeEnum.SMS)
-        return redirect('two-factor')
+    if request.method == "POST":
+        form = PersonalCapitalLoginForm(request.POST)
 
-    return HttpResponse("Index. It didn't work.")
+        if form.is_valid():
+            session = request.session
+            session['pc_email'] = form.cleaned_data['pc_email']
+            session['pc_password'] = form.cleaned_data['pc_password']
+            
+            try:
+                pc.login(session['pc_email'], session['pc_password'])
+            except RequireTwoFactorException:
+                pc.two_factor_challenge(TwoFactorVerificationModeEnum.SMS)
+                return redirect('two-factor')
+
+            accounts = fetch_accounts_json(pc)
+            transactions = fetch_transactions_json(pc)
+            reply = f"Networth: {accounts['spData']['networth']}"
+            reply += "Transaction: "
+            reply += f"{transactions['spData']['transactions'][0]['description']}"
+
+            return HttpResponse(reply)
+
+    else:
+        form = PersonalCapitalLoginForm()
+
+    return render(request, "budget/personal-capital-login.html",\
+            {'form': form})
+
 
 def two_factor(request):
+    """Get two-factor SMS code from user"""
     if request.method == "POST":
         form = SmsAuthenticationForm(request.POST)
         
         if form.is_valid():
             sms_code = form.cleaned_data['sms_code']
-            pc.two_factor_authenticate(TwoFactorVerificationModeEnum.SMS, sms_code)
+            pc.two_factor_authenticate(\
+                    TwoFactorVerificationModeEnum.SMS,\
+                    sms_code)
             pc.authenticate_password(request.session['pc_password'])
 
-            accounts_response = pc.fetch('/newaccount/getAccounts')
-            accounts = accounts_response.json()['spData']
-            reply = 'Networth: {0}'.format(accounts['networth'])
+            accounts = fetch_accounts_json(pc)
+            transactions = fetch_transactions_json(pc)
+            reply = f"Networth: {accounts['spData']['networth']}"
+            reply += "Transaction: "
+            reply += f"{transactions['spData']['transactions'][0]['description']}"
 
             return HttpResponse(reply)
         
         else:
             return HttpResponse('Invalid form')
 
-    else:
+    else: # Via GET
         form = SmsAuthenticationForm()
 
     return render(request, "budget/two-factor.html", {'form': form})
